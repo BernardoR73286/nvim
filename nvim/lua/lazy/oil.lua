85de8596735cc076e639009b8e92963359214232
bernardo <b.rosario@campus.fct.unl.pt>
2025-12-27 20:24:41 +0000

    update oil.nvim config

--- nvim/lua/lazy/oil.lua
+++ nvim/lua/lazy/oil.lua
@@ -10,10 +10,11 @@ return {
             keymaps = {
                 ["<C-h>"] = "actions.parent",
                 ["<C-l>"] = function ()
+                    local actions = require("oil.actions")
                     vim.cmd.WriteOil()
                     actions.select.callback()
                 end,
-
+                "actions.select",
                 ["<M-h>"] = "actions.select_split",
             },
             winopts = {
